919335b92b982ecb10a96fa4d9b7ae6460c7fa37
Florian Ratz <florian.ratz@supsign.ch>
2025-05-02 08:56:59 +0200

    no message

--- src/BaseApi.php
+++ src/BaseApi.php
@@ -127,7 +127,7 @@ class BaseApi
             throw new \Exception('no endpoint was specified');
         }
 
-        return static::class.':'.$this->endpoint.':'.implode('-', $this->requestData);
+        return static::class.':'.$this->endpoint.':'.md5(serialize($this->requestData));
     }
 
     protected function getHeaders(): array
